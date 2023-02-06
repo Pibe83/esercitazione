17cb6943ee9fdf80a119e7d63ed18dc29852cdd6
Pibe83 <123738865+Pibe83@users.noreply.github.com>
2023-02-06 16:41:08 +0100

    Add files via upload

---
+++ w2-d1-esercizio/assets/js/index.js
@@ -0,0 +1,4 @@
+alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
+document.getElementById(titolo).innerHTML;
+document.write("<p>output scritto da javascript</p>")
---
+++ w2-d1-esercizio/assets/js/naming.js
@@ -0,0 +1,24 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+
+var Primopet = "pantera";
+
+
+var Secondopet = "criceto";
+console.log(Secondopet);
+
+var Terzopet = "giraffa";
+console.log(Terzopet);
+
+var Pet = "leone";
+console.log(Pet);
+
+document.write(Primopet + "scritto da javascript");
+
+
+
+//var 12 = 'numero';
+
